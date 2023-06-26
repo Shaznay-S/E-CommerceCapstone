4374db7fc4ec6584d8fad6bbceffb900c6393491
Santiago Osuna <santiago_osuna@icloud.com>
2023-06-26 13:02:56 -0400

    new updates to MySqlCategoryDao

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,11 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -18,25 +21,91 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     }
 
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
+    public List<Category> getAllCategories() {
+        //Create an empty list to store the categories
+        List<Category> categories = new ArrayList<>();
+
+        //Try to establish a connection and execute the SQL query
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM categories");
+             ResultSet resultSet = statement.executeQuery()) {
+
+            //Iterate through the result set
+            while (resultSet.next()) {
+                //Create a Category object by mapping the current row of the result set
+                Category category = mapRow(resultSet);
+
+                //Add the category to the list
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            //Handle any exceptions that occur during the database operations
+            //Print the stack trace for debugging purposes
+            e.printStackTrace();
+        }
+
+        //Return the list of categories
+        return categories;
+    }
+    @Override
+    public Category getById(int categoryId) {
+        //Try to establish a connection and execute the SQL query
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM categories WHERE category_id = ?")) {
+
+            //Set the category_id parameter in the prepared statement
+            statement.setInt(1, categoryId);
+
+            //Execute the query and obtain the result set
+            try (ResultSet resultSet = statement.executeQuery()) {
+                //Check if a category is found
+                if (resultSet.next()) {
+                    //Create a Category object by mapping the current row of the result set
+                    return mapRow(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            //Handle any exceptions that occur during the database operations
+            //Print the stack trace for debugging purposes
+            e.printStackTrace();
+        }
+
+        //If no category is found or an exception occurs, return null
         return null;
     }
 
+
     @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
+    public Category create(Category category) {
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(
+                     "INSERT INTO categories (name, description) VALUES (?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                // Insertion failed
+                // Handle the exception or notify accordingly
+                return null;
+            }
+
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    int categoryId = generatedKeys.getInt(1);
+                    category.setCategoryId(categoryId);
+                    return category;
+                }
+            }
+        } catch (SQLException e) {
+            // Handle the exception appropriately
+            e.printStackTrace();
+        }
         return null;
     }
 
-    @Override
-    public Category create(Category category)
-    {
-        // create a new category
-        return null;
-    }
 
     @Override
     public void update(int categoryId, Category category)
